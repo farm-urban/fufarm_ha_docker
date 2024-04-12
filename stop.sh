287b74281000f35a6409c8982792802937da4fe6
Jens Thomas <linucks42@gmail.com>
2024-04-12 09:29:37 +0100

    UPdate

--- stop.sh
+++ stop.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
 
-if [ $(which docker-compose) ]; then
-  DOCKER_COMPOSE=docker-compose
-elif [ $(which docker compose) ]; then
-  DOCKER_COMPOSE="docker compose"
-else
-  echo ## CANNOT FIND DOCKER COMPOSE
-  exit 1
-fi
-
-${DOCKER_COMPOSE} -f docker-compose.yml down
+docker compose -f docker-compose.yml down
 
