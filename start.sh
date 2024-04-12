22396f3cdab57a86c70ed0d5d101d282c5f7ae04
Jens Thomas <linucks42@gmail.com>
2024-04-12 09:23:53 +0100

    Fixed scripts for different docker versions

--- start.sh
+++ start.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-docker-compose -f docker-compose.yml up -d
+
+if [ $(which docker-compose) ]; then
+  DOCKER_COMPOSE=docker-compose
+elif [ $(which docker compose) ]; then 
+  DOCKER_COMPOSE="docker compose"
+else
+  echo ## CANNOT FIND DOCKER COMPOSE
+  exit 1
+fi
+
+${DOCKER_COMPOSE} -f docker-compose.yml up -d
